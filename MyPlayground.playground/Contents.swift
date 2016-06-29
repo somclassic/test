54a4bfda4056df98d6e23c8bc9a03cdd0dd22140
chkittika@buzzebees.com <deu.w@hotmail.com>
2016-06-29 13:00:25 +0700

    somas

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+if let xx:String = str
+{
+    print(xx)
+}
+
+var strsss = 2
+
+if !strsss.isEmpty{
+    print("dddddd")
+}
